05ee297a03cc75272e67359bfcb93d8bfcb03b19
Wiebe Wiersema <github.com@wiersema.org>
2011-06-19 15:15:34 +0200

    empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoubleLedger::Application.config.secret_token = 'ede13d1c28304543c2ad7607d9a7b97663a313d5c7ae20ef981591156486f2127cc55b1ead4fd3e5a2e97a4351336c5de6c77ef409ee79fc9d3144f54daed2a5'
